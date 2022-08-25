fc413083473149a3870c7698550f8cdc78998fb6
aish-bit <84989206+aish-bit@users.noreply.github.com>
2022-08-25 23:00:52 +0530

    Add files via upload

---
+++ Destructor_In_Cs/Program.cs
@@ -0,0 +1,73 @@
+﻿//referecne link : https://www.c-sharpcorner.com/article/understanding-destructors-in-C-Sharp/
+//destructor internally override finalize method from Super Object class
+//so destructor gets called by gc
+
+using System;
+
+namespace Destructor_In_Cs
+{
+
+    class Base
+    {
+        public Base()
+        {
+            Console.WriteLine("Inside Base default constructor");
+        }
+
+        ~Base() //A Destructor does not accept any parameters and modifiers.
+        {
+            Console.WriteLine("Inside Basedestructor");
+        }
+        public virtual void fun()   
+        {
+            Console.WriteLine("Base fun");
+        }
+
+        public void gun()
+        {
+            Console.WriteLine("Base gun");
+        }
+    }
+
+    class Derived : Base 
+    {
+
+        public Derived()
+        {
+            Console.WriteLine("Inside Derived default constructor");
+        }
+        public override void fun() 
+        {
+            Console.WriteLine("Derived fun");
+        }
+
+        public void sun()
+        {
+            Console.WriteLine("Derived sun");
+        }
+
+        ~Derived() //A Destructor does not accept any parameters and modifiers.
+        {
+            Console.WriteLine("Inside Derived destructor");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Derived dobj = new Derived();
+
+        }
+    }
+}
+
+
+
+
+
+
+
+    
+
+  
+
---
+++ Dynamic_Memory_Allocation_In_Cs/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Dynamic_Memory_Allocation_In_Cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Demo dobj = new Demo();   //same as java and automatic garbage collection
+
+            dobj.fun();
+
+            Console.ReadKey();
+        }
+    }
+
+
+    class Demo
+    {
+        public int x, y;
+
+        public Demo()
+        {
+            Console.WriteLine("Inside default constructor");
+            x = 0;
+            y = 0;
+        }
+
+        public void fun()
+        {
+            Console.WriteLine("Inside fun");
+        }
+    }
+
+
+}
